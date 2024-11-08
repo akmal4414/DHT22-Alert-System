b102727a6c1bc26e3c9e942cacac847f26b3718d
Muhammad Akmal bin Mohamed Sukhairul <muhammadakmal010501@gmail.com>
2024-11-08 17:54:08 +0800

    DHT22_Buzzer.ino

--- DHT22_Buzzer.ino
+++ DHT22_Buzzer.ino
@@ -1,36 +1,36 @@
 #include <DHT.h>
 
-// Pin Definitions
-#define DHT22_PIN 9        // Pin connected to the DHT22 sensor
-#define BUZZER_PIN 4       // Pin connected to the buzzer
-#define GREEN_LED_PIN 5    // Pin connected to the green LED
 
-// DHT Sensor Configuration
+#define DHT22_PIN 9      
+#define BUZZER_PIN 4      
+#define GREEN_LED_PIN 5    
+
+/
 #define DHTTYPE DHT22      // Change this to DHT11 if you're using a DHT11 sensor
 DHT dht(DHT22_PIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
   
-  dht.begin(); // Initialize the DHT sensor
+  dht.begin(); 
 
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
 
-  // Ensure all outputs are initially off
+
   digitalWrite(BUZZER_PIN, LOW);
   digitalWrite(GREEN_LED_PIN, LOW);
 }
 
 void loop() {
-  delay(2000); // Delay between sensor readings
+  delay(2000); 
 
   // Read humidity and temperature
   float humidity = dht.readHumidity();
   float tempC = dht.readTemperature();
   float tempF = dht.readTemperature(true);
 
-  // Check if the readings are valid
+  // Check if the readings are valid or okay
   if (isnan(humidity) || isnan(tempC) || isnan(tempF)) {
     Serial.println("Failed to read from DHT22 sensor!");
     return;
